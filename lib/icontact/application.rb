aa2695e3cd11b97f4063897bfb6dd0127941d4fa
Jon Gilbraith <jon@professionalnerd.com>
2008-10-27 13:58:25 +0000

    Updated the login application class to implement logging in. Tried to write tests, but couldn't figure out a good approach to mocking Net::HTTP, and it didn't seem like there was much point in mocking anything else.

--- lib/icontact/application.rb
+++ lib/icontact/application.rb
@@ -1,6 +1,14 @@
+require 'net/http'
+require 'uri'
+require 'hpricot'
 module Icontact
   
+  class RequestError < StandardError ; end
+  class LoginFail < StandardError ; end
+  
   class Application
+    
+    BASE_URI = "http://api.icontact.com/icp/core/api/v1.0/"
     
     # Your application's API key, retrieved from within the iContact account
     # at http://www.icontact.com/icp/core/registerapp.
@@ -10,10 +18,24 @@ module Icontact
     # at http://www.icontact.com/icp/core/registerapp.
     cattr_accessor :shared_secret
 
-    # This is a string returned by iContact after successfully authenticating
-    # which must be used in all api calls.
-    cattr_accessor :token
-    
+    # Log into the application - returns a session
+    def self.login(username, password)
+      hmac = Digest::MD5.hexdigest("#{self.shared_secret}auth/login/#{username}/#{Digest::MD5.hexdigest("cn1000!")}api_key#{self.api_key}")
+      url = URI.parse(BASE_URI + "auth/login/#{username}/#{Digest::MD5.hexdigest(password)}/?api_key=#{self.api_key}&api_sig=#{hmac}")
+
+      result = Net::HTTP::get_response(url)
+      if result.kind_of?(Net::HTTPSuccess)
+        xml = Hpricot.XML(result.body)
+        if xml.at(:response)[:status] == "fail"
+          raise Icontact::LoginFail
+        elsif xml.at(:response)[:status] == "success"
+          { :token => xml.at(:token).inner_html, :sequence => xml.at(:seq).inner_html.to_i }
+        end
+      else
+        raise Icontact::RequestError, "HTTP Response: #{result.code} #{result.message}"
+      end
+    end
+
   end
   
 end
