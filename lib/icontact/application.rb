1878aedb6c90791f73f42950bf782d7eae69434b
Doug Johnston <dbjohnston@gmail.com>
2009-04-21 18:04:33 -0700

    Add POST functionality and minor refactoring

--- lib/icontact/application.rb
+++ lib/icontact/application.rb
@@ -15,12 +15,12 @@ module Icontact
     def self.login(username, password)
       hmac = Digest::MD5.hexdigest("#{self.shared_secret}auth/login/#{username}/#{Digest::MD5.hexdigest(password)}api_key#{self.api_key}")
       url = URI.parse(BASE_URI + "auth/login/#{username}/#{Digest::MD5.hexdigest(password)}/?api_key=#{self.api_key}&api_sig=#{hmac}")
-
       result = Net::HTTP::get_response(url)
       if result.kind_of?(Net::HTTPSuccess)
         xml = Hpricot.XML(result.body)
         if xml.at(:response)[:status] == "fail"
-          raise Icontact::LoginFail
+          raise xml.inspect
+          # raise Icontact::LoginFail
         elsif xml.at(:response)[:status] == "success"
           application = self.new
           application.token = xml.at(:token).inner_html
@@ -40,6 +40,7 @@ module Icontact
       url = URI.parse(BASE_URI + "contacts?#{(search_query + "&") unless search_query.nil? }api_key=#{self.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
 
       result = Net::HTTP::get_response(url)
+      increment_sequence
       puts result.body
       if result.kind_of?(Net::HTTPSuccess)
         xml = Hpricot.XML(result.body)
@@ -56,6 +57,40 @@ module Icontact
     def contact(id)
       self.instance_variable_get("@contact_#{id}").nil? ? self.instance_variable_set("@contact_#{id}", Icontact::Contact.new(self.token, self.sequence, id)) : self.instance_variable_get("@contact_#{id}")
     end
+    
+    # Create a new contact
+    def create_contact(contact)
+      hmac = Digest::MD5.hexdigest("#{self.class.shared_secret}contactapi_key#{self.class.api_key}api_put#{contact.to_xml}api_seq#{self.sequence}api_tok#{self.token}")
+      url = URI.parse(BASE_URI + "contact?api_key=#{self.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
+      
+      result = Net::HTTP.start(url.host, url.port) {|http| http.send_request('PUT', url.request_uri, contact.to_xml) }
+      increment_sequence
+      if result.kind_of?(Net::HTTPSuccess)
+        xml = Hpricot.XML(result.body)
+        
+        # Return the contact ID for now
+        xml.search("contact")[0].get_attribute(:id)
+      else
+        raise Icontact::RequestError, "HTTP Response: #{result.code} #{result.message}"
+      end
+    end
+    
+    # Subscribe a contact to a list
+    def subscribe(contact_id, list_id)
+      xml = Builder::XmlMarkup.new
+      xml.instruct!
+      xml.subscription( :id => list_id ) do
+        xml.status( 'subscribed' )
+      end
+      
+      hmac = Digest::MD5.hexdigest("#{self.class.shared_secret}contact/#{contact_id}/subscription/#{list_id}api_key#{self.class.api_key}api_put#{xml.target!}api_seq#{self.sequence}api_tok#{self.token}")
+      url = URI.parse(BASE_URI + "contact/#{contact_id}/subscription/#{list_id}?api_key=#{self.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
+      
+      # raise xml.target!.to_s + url.inspect
+      result = Net::HTTP.start(url.host, url.port) {|http| http.send_request('PUT', url.request_uri, xml.target!) }
+      increment_sequence
+      return result.kind_of?(Net::HTTPSuccess)
+    end
 
     # Retrieves an array of all lists associated with this account, each element being an instance of List.
     def lists
@@ -63,6 +98,7 @@ module Icontact
       url = URI.parse(BASE_URI + "lists?api_key=#{self.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
 
       result = Net::HTTP::get_response(url)
+      increment_sequence
       if result.kind_of?(Net::HTTPSuccess)
         xml = Hpricot.XML(result.body)
         xml.at(:lists).search(:list).inject([]) do |lists, list_xml|
@@ -79,6 +115,13 @@ module Icontact
       self.instance_variable_get("@list_#{id}").nil? ? self.instance_variable_set("@list_#{id}", Icontact::List.new(self.token, self.sequence, id)) : self.instance_variable_get("@list_#{id}")
     end
     
+  protected
+  
+    # According to the API documentation, it is necessary to increment the sequence after every API call in a given session
+    # This is one convoluted and poorly documented API.
+    def increment_sequence
+      self.sequence += 1
+    end
   end
   
 end
--- lib/icontact/contact.rb
+++ lib/icontact/contact.rb
@@ -2,11 +2,9 @@ module Icontact
 
   class Contact < Application
 
-    attr_accessor :token
-
-    attr_accessor :sequence
+    attr_accessor :token, :sequence, :id
     
-    attr_accessor :id
+    attr_writer :fname, :lname, :email, :zip
 
     # Not meant for direct instansiation, either retrieve contacts as an element of application.contacts,
     # or by lookup against the application with application.contact(id).
@@ -18,82 +16,97 @@ module Icontact
 
     # The first name of this contact
     def fname
-      @fname ? @fname : @fname = Hpricot.XML(self.xml).at(:contact).at(:fname)
+      @fname ||= retrieve_contact_field(:fname)
     end
 
     # The last name of this contact
     def lname
-      @lname ? @lname : @lname = Hpricot.XML(self.xml).at(:contact).at(:lname)
+      @lname ||= retrieve_contact_field(:lname)
     end
     
     # The email address of this contact
     def email
-      @email ? @email : @email = Hpricot.XML(self.xml).at(:contact).at(:email)
+      @email ||= retrieve_contact_field(:email)
     end
     
     # This contact's preferred prefix - Mr, Mrs, etc
     def prefix
-      @prefix ? @prefix : @prefix = Hpricot.XML(self.xml).at(:contact).at(:prefix)
+      @prefix ||= retrieve_contact_field(:prefix)
     end
     
     # This contact's suffix, if they have one - phd, esq, etc
     def suffix
-      @suffix ? @suffix : @suffix = Hpricot.XML(self.xml).at(:contact).at(:suffix)
+      @suffix ||= retrieve_contact_field(:suffix)
     end
     
     # This contact's business name
     def business
-      @business ? @business : @business = Hpricot.XML(self.xml).at(:contact).at(:business)
+      @business ||= retrieve_contact_field(:business)
     end
     
     # The first line of this contact's address
     def address1
-      @address1 ? @address1 : @address1 = Hpricot.XML(self.xml).at(:contact).at(:address1)
+      @address1 ||= retrieve_contact_field(:address1)
     end
     
     # The second line of this contact's address
     def address2
-      @address2 ? @address2 : @address2 = Hpricot.XML(self.xml).at(:contact).at(:address2)
+      @address2 ||= retrieve_contact_field(:address2)
     end
     
     # The city from this contact's address
     def city
-      @city ? @city : @city = Hpricot.XML(self.xml).at(:contact).at(:city)
+      @city ||= retrieve_contact_field(:city)
     end
     
     # The state / county of this contact's address
     def state
-      @state ? @state : @state = Hpricot.XML(self.xml).at(:contact).at(:state)
+      @state ||= retrieve_contact_field(:state)
     end
     
     # The zip / postal code of this contact's address
     def zip
-      @zip ? @zip : @zip = Hpricot.XML(self.xml).at(:contact).at(:zip)
+      @zip ||= retrieve_contact_field(:zip)
     end
     
     # This contact's phone number
     def phone
-      @phone ? @phone : @phone = Hpricot.XML(self.xml).at(:contact).at(:phone)
+      @phone ||= retrieve_contact_field(:phone)
     end
     
     # This contact's fax number
     def fax
-      @fax ? @fax : @fax = Hpricot.XML(self.xml).at(:contact).at(:fax)
+      @fax ||= retrieve_contact_field(:fax)
     end
     
     # Retrieves the xml for this contact. Memoized.
     def xml
-      if @xml
-        @xml
-      else
-        hmac = Digest::MD5.hexdigest("#{self.class.shared_secret}contact/#{self.id}api_key#{self.class.api_key}api_seq#{self.sequence}api_tok#{self.token}")
-        url = URI.parse(BASE_URI + "contact/#{self.id}?api_key=#{self.class.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
-
-        result = Net::HTTP::get_response(url)
-        result.kind_of?(Net::HTTPSuccess) ? @xml = result.body : raise(Icontact::RequestError, "HTTP Response: #{result.code} #{result.message}")
+      @xml ||= retrieve_contact
+    end
+    
+    # Prepare the contact for a PUT
+    def to_xml
+      xml = Builder::XmlMarkup.new
+      xml.instruct!
+      xml.contact( :id => "") do
+        xml.email( @email )
+        xml.zip( @zip )
       end
+      return xml.target!
     end
   
+  private
+    def retrieve_contact_field(field)
+      Hpricot.XML(self.xml).at(:contact).at(field) || nil
+    end
+
+    def retrieve_contact
+      hmac = Digest::MD5.hexdigest("#{self.class.shared_secret}contact/#{self.id}api_key#{self.class.api_key}api_seq#{self.sequence}api_tok#{self.token}")
+      url = URI.parse(BASE_URI + "contact/#{self.id}?api_key=#{self.class.api_key}&api_seq=#{self.sequence}&api_tok=#{self.token}&api_sig=#{hmac}")
+
+      result = Net::HTTP::get_response(url)
+      increment_sequence
+      result.kind_of?(Net::HTTPSuccess) ? result.body : raise(Icontact::RequestError, "HTTP Response: #{result.code} #{result.message}")
+    end
   end
- 
 end
